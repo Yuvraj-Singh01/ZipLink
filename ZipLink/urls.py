a4507deca0a3dbc27362e1c6b90b893d2bc702ee
Yuvraj <yuvsingh18@gmail.com>
2024-12-07 19:31:38 +0530

    Added login page and Authentication

--- ZipLink/urls.py
+++ ZipLink/urls.py
@@ -4,8 +4,5 @@ from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name ='home'),
-    path('login/',views.login,name = 'login'),
-    path('shorten/',views.shorten_url, name = 'shorten_url'),
-    path('<str:shortened_id>/', views.redirect_to_original, name = 'redirect_to_original'),
+    path('', include('base.urls')),
 ]
--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index'),  # Homepage
-    path('login/', views.login, name = 'login'),  # Login page
+    path('', views.index, name = 'index'),
+    path('login/', views.user_login, name = 'login'),
     path('shorten/', views.shorten_url, name = 'shorten_url'),  # Shorten URL logic
     path('<str:shortened_id>/', views.redirect_to_original, name = 'redirect_to_original'),  # Redirect to original URL
+    path("userHome/", views.userHome, name="userHome"),
 ]
