6d7e1c343c88821b7e02a6af57255039e5a21074
Deepali <122445268+lavenderpeaches@users.noreply.github.com>
2024-12-07 20:46:28 +0530

    Updated the home button

--- ZipLink/urls.py
+++ ZipLink/urls.py
@@ -4,9 +4,5 @@ from base import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name ='home'),
-    path('login/',views.login,name = 'login'),
-    path('shorten/',views.shorten_url, name = 'shorten_url'),
-     path('api/shorten/', views.shorten_url, name='shorten_url'),  
-    path('<str:short_code>/', views.redirect_to_url, name='redirect_url'),
+    path('',include('base.urls')),
 ]
